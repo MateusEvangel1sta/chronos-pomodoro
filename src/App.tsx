2272f8cbb815ee00c04577d7d09983e73fdbfcc9
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-06-07 11:42:13 -0400

    :lipstick: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -46,9 +47,12 @@ export function App() {
 
           <div className='formRow'>
             <DefaultButton icon={<PlayCircleIcon />} />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
